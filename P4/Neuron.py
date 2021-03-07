dc1969e88573e5870c557940d10b0d0d6f197932
Jellestiesri <jellestiesri@gmail.com>
2021-03-07 16:17:09 +0100

    P4 calculate Error

--- P4/Neuron.py
+++ P4/Neuron.py
@@ -4,6 +4,7 @@ from typing import List
 class Neuron:
 
     def __init__(self, name: str, bias: float, weights: List[float]):
+        self.error = None
         self.name = name
         self.bias = bias
         self.weights = weights
@@ -21,21 +22,39 @@ class Neuron:
 
         return self.sigmoid(weighted_sum)
 
-    def error(self, expectation: ([], bool)): # OF FLOAT/BOOL
-        for input_list, target in expectation:
-            print(input_list, target)
-            # derivative =
-
-
-    # def error(self, expectations: ([], bool)):
-    #     """Berekent de totale error over alle trainingsvoorbeelden
-    #     Formule: MSE = Σ | d – y |2 / n"""
-    #     error = 0
-    #     for input_list, expectation in expectations:
-    #         error += (expectation - self.activation(input_list))**2
+    # def calculate_error(self, expectation: [[], bool]):
+    #     for input_list, target in [expectation]:
+    #         derivative = self.activation(input_list) * (1 - self.activation(input_list))
+    #         # print(f'Afgeleide {derivative}\n')
+    #         error = derivative * -(target - self.activation(input_list))
+    #         # print(f'Error: {error}')
     #
-    #     return error / len(expectations)
+    #         self.error = error
 
+    def calculate_error(self, output: float,  target: float):
+        derivative = output * (1 - output)
+        error = derivative * -(target - output)
+
+        self.error = error
+
+    def update(self, learning_rate: float = 0.1):
+        self.bias -= (learning_rate*self.error)
+
+        pass
+
+    # def update(self, expectations: [[], bool]):
+    #     for input_list, expectation in expectations:
+    #         output = self.activation(input_list)
+    #         print(f'input: {input_list, expectation} --- Output: {output}')
+    #
+    #         self.calculate_error(output, expectation)
+    #         print(f'Error: {self.error}')
+    #
+    #         gradient = output * self.error
+    #         print(f'Gradient: {gradient}')
+    #
+    #
+    #         print("\n")
 
     def __str__(self):
         return f'<{self.name} port>\n' \
--- P4/TestP4.py
+++ P4/TestP4.py
@@ -1,3 +1,30 @@
 from Neuron import *
+import random
 
-N1 = Neuron('Neuron 1', 1.2, [0.5, 2.1])
+N1 = Neuron('Neuron 1', -1.2, [0.5, 0.1])
+
+AND_expectations = [[[False, False], False],
+                    [[False, True], False],
+                    [[True, False], False],
+                    [[True, True], True]]
+
+print('Verwachting', AND_expectations[1])
+print(N1)
+# print('Activation:', N1.activation(AND_expectations[1][0]))
+
+# N1.calculate_error(AND_expectations[1])
+# N1.update(AND_expectations)
+
+# random.shuffle(AND_expectations)
+# print(AND_expectations)
+
+for i in range(1):
+    for input_list, expectation in AND_expectations:
+
+        activation = N1.activation(input_list)
+        print(f'input: {input_list, expectation} --- Output: {activation}')
+        N1.calculate_error(activation, expectation)
+        print('error ',N1.error)
+        N1.update()
+
+        print('\n')
